650173c05cdc252d0521a505958041573a1bb513
Lucas <lojavirtual@tintasverginia.com.br>
2022-07-22 14:42:54 -0300

    script teste

--- shx.js
+++ shx.js
@@ -1,3 +1,6 @@
 var chaveNota = document.getElementsByTagName("title")[0].textContent
         
-nps =  chaveNota.replace(/\D/gim, '').replace(/[^0-9]/g,'')
+//nps =  chaveNota.replace(/\D/gim, '').replace(/[^0-9]/g,'')
+
+
+console.log(chaveNota, "teste aqui")
